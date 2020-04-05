e390d2a6532d4b731b97c513d8cf7bae9841f0ec
Arnaud <arnaud.derbal@gmail.com>
2020-04-05 15:08:26 +0200

    Implements SkeletonTest and GoldDragonTest

--- src/test/java/model/realcharacters/GoldDragonTest.java
+++ src/test/java/model/realcharacters/GoldDragonTest.java
@@ -1,4 +1,50 @@
 package model.realcharacters;
 
+import fr.sogeti.rpgapp.model.Creature;
+import fr.sogeti.rpgapp.model.characteroptions.Dragon;
+import fr.sogeti.rpgapp.model.characteroptions.MagicCaster;
+import fr.sogeti.rpgapp.model.characteroptions.Monster;
+import fr.sogeti.rpgapp.model.realcharacters.GoldDragon;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class GoldDragonTest {
+    Creature target;
+    GoldDragon goldDragon;
+
+    @Before
+    public void beforeEach(){
+        target = Mockito.mock(Creature.class, Mockito.CALLS_REAL_METHODS);
+        Mockito.when(target.getLevel()).thenReturn(1);
+        Mockito.when(target.getIntelligence()).thenReturn(10);
+        Mockito.when(target.getHealthPoints()).thenReturn(100);
+        goldDragon = new GolDragon(1000, 20, [15,10,12]);
+    }
+
+    @Test
+    public void testDefaultConstructorValues(){
+        Assert.assertEquals("Gold Dragon",goldDragon.getName());
+        Assert.assertEquals(100000000, goldDragon.getExpReward());
+        Assert.assertEquals("Legendary item", goldDragon.getLoot());
+        Assert.assertTrue(goldDragon.isDodging());
+    }
+
+    @Test
+    public void testVampireExtendsCharacter(){
+        Assert.assertTrue(goldDragon instanceof Creature);
+    }
+
+    @Test
+    public void testClassImplementsCorrectInterfaces(){
+        Assert.assertTrue(goldDragon instanceof Monster);
+        Assert.assertTrue(goldDragon instanceof Dragon);
+        Assert.assertTrue(goldDragon instanceof MagicCaster);
+    }
+
+    @Test
+    public void testGoldDragonMakeSound(){
+        Assert.assertEquals("groooow",skeleton.makeSound());
+    }
 }
--- src/test/java/model/realcharacters/SkeletonTest.java
+++ src/test/java/model/realcharacters/SkeletonTest.java
@@ -1,4 +1,47 @@
 package model.realcharacters;
 
+import fr.sogeti.rpgapp.model.Creature;
+import fr.sogeti.rpgapp.model.characteroptions.Monster;
+import fr.sogeti.rpgapp.model.characteroptions.Undead;
+import fr.sogeti.rpgapp.model.realcharacters.Skeleton;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class SkeletonTest {
+    Creature target;
+    Skeleton skeleton;
+
+    @Before
+    public void beforeEach(){
+        target = Mockito.mock(Creature.class, Mockito.CALLS_REAL_METHODS);
+        Mockito.when(target.getLevel()).thenReturn(1);
+        Mockito.when(target.getIntelligence()).thenReturn(10);
+        Mockito.when(target.getHealthPoints()).thenReturn(100);
+        skeleton = new Skeleton(1, 1, [2,1,1]);
+    }
+
+    @Test
+    public void testDefaultConstructorValues(){
+        Assert.assertEquals("Skeleton",skeleton.getName());
+        Assert.assertEquals(5, skeleton.getExpReward());
+        Assert.assertFalse(skeleton.isDodging());
+    }
+
+    @Test
+    public void testVampireExtendsCharacter(){
+        Assert.assertTrue(skeleton instanceof Creature);
+    }
+
+    @Test
+    public void testClassImplementsCorrectInterfaces(){
+        Assert.assertTrue(skeleton instanceof Monster);
+        Assert.assertTrue(skeleton instanceof Undead);
+    }
+
+    @Test
+    public void testGoldDragonMakeSound(){
+        Assert.assertEquals("klklkl",skeleton.makeSound());
+    }
 }
