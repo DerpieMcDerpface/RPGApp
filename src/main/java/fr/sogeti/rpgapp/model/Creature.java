19d707ec864f395441ac91a4038ab1f01a30e37d
Benjamin Mouginot <benjamin.mouginot@live.fr>
2020-04-05 09:52:40 +0200

    Implementation of Creature
    * First implementation of Creature.class
    * Some methods where added compared to our initial conception plan : 
    - takeDamage(), heal()
    - getters and setters for the basic stats attribute

--- src/main/java/fr/sogeti/rpgapp/model/Creature.java
+++ src/main/java/fr/sogeti/rpgapp/model/Creature.java
@@ -1,8 +1,123 @@
 package fr.sogeti.rpgapp.model;
 
-public class Creature {
+import fr.sogeti.rpgapp.model.characteroptions.Undead;
 
-    
+public abstract class Creature {
 
+    private String name;
+    private int maximumHealth;
+    private int healthPoints;
+    private int level;
+    private int[] stats;
+    private boolean isDodging;
 
+    protected Creature(String name, int healthPoints, int level, int[] stats){
+        this.name = name;
+        if(healthPoints>0) {
+            this.healthPoints = healthPoints;
+            this.maximumHealth = healthPoints;
+        } else throw new IllegalArgumentException("Error illegal healthPoints argument, must be higher than zero");
+        this.level = level;
+        this.isDodging = false;
+
+        if(stats.length == 3 && (stats[0]>0 && stats[1]>0 && stats[2]>0)){
+            this.stats = stats;
+        } else {
+            throw new IllegalArgumentException("Error illegal 'stats' argument, stats chould contain 3 positive int values.");
+        }
+    }
+
+    public void attack(Creature target){
+        int attackDamage = 0;
+        attackDamage += this.getLevel() + this.getStrength() - target.getLevel();
+
+        if(target.isDodging()) attackDamage -= target.getAgility();
+        if(target instanceof Undead) attackDamage /= 2;
+        if(attackDamage < 0) attackDamage = 0;
+
+        target.takeDamage(attackDamage);
+    }
+
+    public void dodge(){
+        this.isDodging = true;
+    }
+
+    public void takeDamage(int damage){
+        if(damage >= 0){
+            this.healthPoints -= damage;
+        } else {
+            throw new IllegalArgumentException("Error illegal damage argument, damage can't be negative");
+        }
+
+    }
+
+    public void heal(int healAmount){
+        if(healAmount > 0){
+            if(this.healthPoints+healAmount>this.maximumHealth) this.healthPoints = this.maximumHealth;
+            else this.healthPoints += healAmount;
+        } else {
+            throw new IllegalArgumentException("Error illegal healAmount argument, can't heal for a negative value");
+        }
+    }
+
+    public int getStrength(){
+        return this.stats[0];
+    }
+
+    public void setStrength(int strength){
+        if(strength >= 0){
+            this.stats[0] = strength;
+        } else {
+            throw new IllegalArgumentException("Error illegal argument, stats can't be negative");
+        }
+    }
+
+    public int getIntelligence(){
+        return this.stats[1];
+    }
+
+    public void setIntelligence(int intelligence){
+        if(intelligence >= 0){
+            this.stats[1] = intelligence;
+        } else {
+            throw new IllegalArgumentException("Error illegal argument, stats can't be negative");
+        }
+    }
+
+    public int getAgility(){
+        return this.stats[2];
+    }
+
+    public void setAgility(int agility){
+        if(agility >= 0){
+            this.stats[2] = agility;
+        } else {
+            throw new IllegalArgumentException("Error illegal argument, stats can't be negative");
+        }
+    }
+
+    //CLASSIC GETTERS & SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int[] getStats() {
+        return stats;
+    }
+
+    public boolean isDodging() {
+        return isDodging;
+    }
 }
