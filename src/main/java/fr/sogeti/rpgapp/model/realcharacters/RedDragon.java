6c9403dbbbf1dc184ef65fdc593c9a826c3f1c54
Ilyasse <ilyasse.tissafiidrissi@gmail.com>
2020-04-06 13:43:59 +0200

    Fixed some copy pasta fails and now we can build it

--- src/main/java/fr/sogeti/rpgapp/model/realcharacters/RedDragon.java
+++ src/main/java/fr/sogeti/rpgapp/model/realcharacters/RedDragon.java
@@ -1,4 +1,38 @@
 package fr.sogeti.rpgapp.model.realcharacters;
 
-public class RedDragon {
+import fr.sogeti.rpgapp.model.Creature;
+import fr.sogeti.rpgapp.model.characteroptions.Dragon;
+import fr.sogeti.rpgapp.model.characteroptions.Monster;
+
+public class RedDragon extends Creature implements Dragon, Monster {
+
+    private String loot;
+    private static final int xpReward = 5;
+
+    protected RedDragon(String name, int healthPoints, int level, int[] stats, String loot) {
+        super(name, healthPoints, level, stats);
+        this.loot = loot;
+    }
+
+    @Override
+    public void breath() {
+
+    }
+
+    @Override
+    public void makeSound() {
+
+    }
+
+    public String getLoot() {
+        return this.loot;
+    }
+
+    public int getExpReward() {
+        return this.xpReward;
+    }
+
+    public void setLoot(String loot) {
+        this.loot = loot;
+    }
 }
